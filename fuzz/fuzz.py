5e0952a7991e1e1bfa2302636a1a77ad4c599d2e
ebell495 <ericbell495@gmail.com>
2022-05-24 11:42:56 -0700

    Integrate Mayhem

---
+++ fuzz/fuzz.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+import atheris
+import sys
+
+import pkg_resources
+from symspellpy import SymSpell, Verbosity
+
+sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+dictionary_path = pkg_resources.resource_filename(
+    "symspellpy", "frequency_dictionary_en_82_765.txt"
+)
+bigram_path = pkg_resources.resource_filename(
+    "symspellpy", "frequency_bigramdictionary_en_243_342.txt"
+)
+# term_index is the column of the term and count_index is the
+# column of the term frequency
+sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)
+
+@atheris.instrument_func
+def TestOneInput(data):
+    try:
+        input_term = data.decode("utf-8")
+    except UnicodeDecodeError:
+        return
+    
+    sym_spell.lookup_compound(input_term, max_edit_distance=2)
+
+
+atheris.instrument_all()
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
